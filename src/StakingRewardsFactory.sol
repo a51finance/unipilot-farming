8015e8bc82eaceac952da4dd416ec2ca1b6b535c
anassohail99 <anas.sohailabs1@gmail.com>
2023-01-03 15:10:00 +0500

    fix(file): add virtual override to function

--- src/StakingRewardsFactory.sol
+++ src/StakingRewardsFactory.sol
@@ -6,48 +6,13 @@ import "./interfaces/SafeERC20.sol";
 import "./libraries/SafeMath.sol";
 import "./libraries/Math.sol";
 import "openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "openzeppelin/contracts/access/Ownable.sol";
 
-/**
- * @dev Contract module that helps prevent reentrant calls to a function.
- *
- * Inheriting from `ReentrancyGuard` will make the `nonReentrant` modifier
- * available, which can be aplied to functions to make sure there are no nested
- * (reentrant) calls to them.
- *
- * Note that because there is a single `nonReentrant` guard, functions marked as
- * `nonReentrant` may not call one another. This can be worked around by making
- * those functions `private`, and then adding `external` `nonReentrant` entry
- * points to them.
- */
-// abstract contract ReentrancyGuard {
-//     /// @dev counter to allow mutex lock with only one SSTORE operation
-//     uint256 private _guardCounter;
-
-//     constructor () internal {
-//         // The counter starts at one to prevent changing it from zero to a non-zero
-//         // value, which is a more expensive operation.
-//         _guardCounter = 1;
-//     }
-
-//     /**
-//      * @dev Prevents a contract from calling itself, directly or indirectly.
-//      * Calling a `nonReentrant` function from another `nonReentrant`
-//      * function is not supported. It is possible to prevent this from happening
-//      * by making the `nonReentrant` function external, and make it call a
-//      * `private` function that does the actual work.
-//      */
-//     modifier nonReentrant() {
-//         _guardCounter += 1;
-//         uint256 localCounter = _guardCounter;
-//         _;
-//         require(localCounter == _guardCounter, "ReentrancyGuard: reentrant call");
-//     }
-// }
 
 abstract contract RewardsDistributionRecipient {
     address public rewardsDistribution;
 
-    function notifyRewardAmount(uint256 reward, uint256 duration) external virtual;
+    function notifyRewardAmount(uint256 reward, uint256 duration) virtual external;
 
     modifier onlyRewardsDistribution() {
         require(msg.sender == rewardsDistribution, "Caller is not RewardsDistribution contract");
@@ -88,19 +53,19 @@ contract StakingRewards is IStakingRewards, RewardsDistributionRecipient, Reentr
 
     /* ========== VIEWS ========== */
 
-    function totalSupply() external view override returns (uint256) {
+    function totalSupply() external override  view returns (uint256) {
         return _totalSupply;
     }
 
-    function balanceOf(address account) external view override returns (uint256) {
+    function balanceOf(address account) external override view returns (uint256) {
         return _balances[account];
     }
 
-    function lastTimeRewardApplicable() public view override returns (uint256) {
+    function lastTimeRewardApplicable() public override view returns (uint256) {
         return Math.min(block.timestamp, periodFinish);
     }
 
-    function rewardPerToken() public view override returns (uint256) {
+    function rewardPerToken() public override view returns (uint256) {
         if (_totalSupply == 0) {
             return rewardPerTokenStored;
         }
@@ -110,7 +75,7 @@ contract StakingRewards is IStakingRewards, RewardsDistributionRecipient, Reentr
             );
     }
 
-    function earned(address account) public view  override returns (uint256) {
+    function earned(address account) public override view returns (uint256) {
         return _balances[account].mul(rewardPerToken().sub(userRewardPerTokenPaid[account])).div(1e18).add(rewards[account]);
     }
 
@@ -128,7 +93,7 @@ contract StakingRewards is IStakingRewards, RewardsDistributionRecipient, Reentr
         emit Staked(msg.sender, amount);
     }
 
-    function stake(uint256 amount) external override  nonReentrant updateReward(msg.sender) {
+    function stake(uint256 amount) external override nonReentrant updateReward(msg.sender) {
         require(amount > 0, "Cannot stake 0");
         _totalSupply = _totalSupply.add(amount);
         _balances[msg.sender] = _balances[msg.sender].add(amount);
@@ -153,7 +118,7 @@ contract StakingRewards is IStakingRewards, RewardsDistributionRecipient, Reentr
         }
     }
 
-    function exit()  external override {
+    function exit() external override {
         withdraw(_balances[msg.sender]);
         getReward();
     }
@@ -162,7 +127,7 @@ contract StakingRewards is IStakingRewards, RewardsDistributionRecipient, Reentr
 
     function notifyRewardAmount(uint256 reward, uint256 rewardsDuration) external override onlyRewardsDistribution updateReward(address(0)) {
         require(block.timestamp.add(rewardsDuration) >= periodFinish, "Cannot reduce existing period");
-
+        
         if (block.timestamp >= periodFinish) {
             rewardRate = reward.div(rewardsDuration);
         } else {
@@ -202,7 +167,95 @@ contract StakingRewards is IStakingRewards, RewardsDistributionRecipient, Reentr
     event Withdrawn(address indexed user, uint256 amount);
     event RewardPaid(address indexed user, uint256 reward);
 }
-
 interface IUniswapV2ERC20 {
     function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
+}
+
+contract StakingRewardsFactory is Ownable {
+    // immutables
+    address public rewardsToken;
+    uint public stakingRewardsGenesis;
+
+    // the staking tokens for which the rewards contract has been deployed
+    address[] public stakingTokens;
+
+    // info about rewards for a particular staking token
+    struct StakingRewardsInfo {
+        address stakingRewards;
+        uint rewardAmount;
+        uint duration;
+    }
+
+    // rewards info by staking token
+    mapping(address => StakingRewardsInfo) public stakingRewardsInfoByStakingToken;
+
+    constructor(
+        address _rewardsToken,
+        uint _stakingRewardsGenesis
+    ) Ownable() public {
+        require(_stakingRewardsGenesis >= block.timestamp, 'StakingRewardsFactory::constructor: genesis too soon');
+
+        rewardsToken = _rewardsToken;
+        stakingRewardsGenesis = _stakingRewardsGenesis;
+    }
+
+    ///// permissioned functions
+
+    // deploy a staking reward contract for the staking token, and store the reward amount
+    // the reward will be distributed to the staking reward contract no sooner than the genesis
+    function deploy(address stakingToken, uint rewardAmount, uint256 rewardsDuration) public onlyOwner {
+        StakingRewardsInfo storage info = stakingRewardsInfoByStakingToken[stakingToken];
+        require(info.stakingRewards == address(0), 'StakingRewardsFactory::deploy: already deployed');
+
+        info.stakingRewards = address(new StakingRewards(/*_rewardsDistribution=*/ address(this), rewardsToken, stakingToken));
+        info.rewardAmount = rewardAmount;
+        info.duration = rewardsDuration;
+        stakingTokens.push(stakingToken);
+    }
+
+    function update(address stakingToken, uint rewardAmount, uint256 rewardsDuration) public onlyOwner {
+        StakingRewardsInfo storage info = stakingRewardsInfoByStakingToken[stakingToken];
+        require(info.stakingRewards != address(0), 'StakingRewardsFactory::update: not deployed');
+
+        info.rewardAmount = rewardAmount;
+        info.duration = rewardsDuration;
+    }
+
+
+
+    ///// permissionless functions
+
+    // call notifyRewardAmount for all staking tokens.
+    function notifyRewardAmounts() public {
+        require(stakingTokens.length > 0, 'StakingRewardsFactory::notifyRewardAmounts: called before any deploys');
+        for (uint i = 0; i < stakingTokens.length; i++) {
+            notifyRewardAmount(stakingTokens[i]);
+        }
+    }
+
+    // notify reward amount for an individual staking token.
+    // this is a fallback in case the notifyRewardAmounts costs too much gas to call for all contracts
+    function notifyRewardAmount(address stakingToken) public {
+        require(block.timestamp >= stakingRewardsGenesis, 'StakingRewardsFactory::notifyRewardAmount: not ready');
+
+        StakingRewardsInfo storage info = stakingRewardsInfoByStakingToken[stakingToken];
+        require(info.stakingRewards != address(0), 'StakingRewardsFactory::notifyRewardAmount: not deployed');
+
+        if (info.rewardAmount > 0 && info.duration > 0) {
+            uint rewardAmount = info.rewardAmount;
+            uint256 duration = info.duration;
+            info.rewardAmount = 0;
+            info.duration = 0;
+
+            require(
+                IERC20(rewardsToken).transfer(info.stakingRewards, rewardAmount),
+                'StakingRewardsFactory::notifyRewardAmount: transfer failed'
+            );
+            StakingRewards(info.stakingRewards).notifyRewardAmount(rewardAmount, duration);
+        }
+    }
+
+    function pullExtraTokens(address token, uint256 amount) external onlyOwner {
+        IERC20(token).transfer(msg.sender, amount);
+    }
 }
